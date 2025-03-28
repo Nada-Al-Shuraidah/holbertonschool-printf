bcb2c1718adb062687bf996be826cce3f52073d6
Nada-Al-Shuraidah <10718@holbertonstudents.com>
2025-03-28 11:58:13 +0000

    Fix _printf function

--- printf.c
+++ printf.c
@@ -3,29 +3,66 @@
 #include <unistd.h>
 #include <string.h>
 
+/* Helper function to handle characters */
+/**
+ * handle_char - Prints a single character
+ * @args: A list of arguments pointing to the character to print
+ *
+ * Return: Number of characters printed (always 1)
+ */
+int handle_char(va_list args)
+{
+char c = va_arg(args, int);
+return (write(1, &c, 1));
+}
+
+/* Helper function to handle strings */
+/**
+ * handle_string - Prints a string
+ * @args: A list of arguments pointing to the string to print
+ *
+ * Return: Number of characters printed
+ */
+int handle_string(va_list args)
+{
+char *str = va_arg(args, char *);
+if (!str)
+str = "(null)";
+return (write(1, str, strlen(str)));
+}
+
+/* Helper function to handle %% */
+/**
+ * handle_percent - Prints the percentage symbol '%'
+ *
+ * Return: Number of characters printed (always 1)
+ */
+int handle_percent(void)
+{
+return (write(1, "%", 1));
+}
+
+/* Main function _printf */
 /**
  * _printf - Produces output according to a specific format
  * @format: A string containing the format specifiers and text
  *
  * Description:
- * This function is used to print formatted text to the standard output
- * (stdout). It takes a format string as input and processes directives
- * within the format string to produce the desired output. The following
- * conversion specifiers are supported:
- * - %c: Prints a single character.
- * - %s: Prints a string.
- * - %%: Prints a percent symbol.
+ * This function is used to produce output to the standard output (stdout).
+ * It processes the format string and handles conversion specifiers like:
+ * - %c: Prints a single character
+ * - %s: Prints a string
+ * - %%: Prints the percentage symbol
  *
- * Return:
- * The function returns the total number of characters printed to the output.
+ * Return: Total number of characters printed
  */
-
-
 int _printf(const char *format, ...)
 {
 va_list args;
-int count = 0; /* Number of printed characters */
-char *str;
+int count = 0;
+
+if (!format)
+return (-1);
 
 va_start(args, format);
 
@@ -34,28 +71,18 @@ while (*format)
 if (*format == '%')
 {
 format++;
-if (*format == '\0') /* If the end of the string */
-{
-return (-1); /* Error % cannot be processed */
-}
 switch (*format)
 {
-case 'c': /* Letter printing */
-{
-char c = va_arg(args, int);
-count += write(1, &c, 1);
+case 'c':
+count += handle_char(args);
 break;
-}
-case 's': /* Ptint a text string */
-str = va_arg(args, char *);
-if (!str) /*If the string NULL */
-str = "(null)";
-count += write(1, str, strlen(str));
+case 's':
+count += handle_string(args);
 break;
-case '%': /* Print */
-count += write(1, "%", 1);
+case '%':
+count += handle_percent();
 break;
-default: /* If the format is nit supported, such as %K or %! */
+default:
 count += write(1, "%", 1);
 count += write(1, format, 1);
 break;
